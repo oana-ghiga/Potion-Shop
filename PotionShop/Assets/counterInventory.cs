9f94b9e09b4808af5cf56484637d98f628ed0549
bianca <biancaelena646@gmail.com>
2024-02-07 13:59:52 +0200

    InventoryAndShop

---
+++ PotionShop/Assets/counterInventory.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class counterInventory : MonoBehaviour
+{
+    [SerializeField]
+    private Text _title;
+
+    private int counter = 0;
+
+    private void Start()
+    {
+        _title.text = "0";
+    }
+
+    public void OnButtonClick()
+    {
+        if (counter > 0)
+        {
+            counter--;
+
+            _title.text = counter.ToString();
+        }
+    }
+
+    public void IncrementCounter()
+    {
+        counter++;
+
+        _title.text = counter.ToString();
+    }
+}
---
+++ PotionShop/Assets/shop.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class shop : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject gameManager;
+
+    [SerializeField]
+    private GameObject gameManagerCoins;
+
+    [SerializeField]
+    private Text price;
+
+    public void OnButtonClicked()
+    {
+        if (gameManager != null)
+        {
+            counterInventory counterScript = gameManager.GetComponent<counterInventory>();
+            moneyManagerScript counterMoney = gameManagerCoins.GetComponent<moneyManagerScript>();
+
+            if (counterScript != null)
+            {
+                counterScript.IncrementCounter();
+                counterMoney.SubtractMoney(ConvertStringWithoutLastCharToInt(price.text));
+            }
+            else
+            {
+                Debug.LogError("counterInventory script not found on GameManager.");
+            }
+        }
+        else
+        {
+            Debug.LogError("GameManager is not assigned to the shop script.");
+        }
+
+    }
+
+    static int ConvertStringWithoutLastCharToInt(string input)
+    {
+        if (input.Length > 0)
+        {
+            string substring = input.Substring(0, input.Length - 1);
+
+            if (int.TryParse(substring, out int result))
+            {
+                return result;
+            }
+        }
+        return 0;
+    }
+}
